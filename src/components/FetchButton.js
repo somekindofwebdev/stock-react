f1b1449d1f6f75a8f04e2246b28eddca0febd2f8
Rich Wortham <richard.wortham@pm.me>
2025-04-12 21:15:45 +0100

    Use the proper React method for iterating over data in the table component

--- src/components/FetchButton.js
+++ src/components/FetchButton.js
@@ -1,35 +1,6 @@
 export default function FetchButton({ tableId }) {
   function handleClick() {
-    fetch('http://localhost:8000/animals')
-      .then(response => response.json())
-      .then(json => {
-        let data = JSON.parse(json);
-        if (data.error) {
-          console.log(json.message);
-          return;
-        }
 
-        let table = document.getElementById(tableId);
-        let thead = table.querySelector('thead tr');
-        thead.innerHTML = '';
-        data.headers.forEach(header => {
-          let th = document.createElement('th');
-          th.textContent = header;
-          thead.append(th);
-        });
-
-        let tbody = table.querySelector('tbody');
-        tbody.innerHTML = '';
-        data.data.forEach(dataRow => {
-          let tr = document.createElement('tr');
-          dataRow.forEach(dataPoint => {
-            let td = document.createElement('td');
-            td.textContent = dataPoint;
-            tr.append(td);
-          })
-          tbody.append(tr);
-        });
-      });
   }
 
   return (
--- src/components/StockTable.js
+++ src/components/StockTable.js
@@ -1,19 +1,43 @@
-import FetchButton from "./FetchButton";
+import { useState } from 'react';
 
 export default function StockTable({ id }) {
-  return <div>
+  const [headers, setHeaders] = useState([]);
+  const [dataRows, setDataRows] = useState([]);
+
+  async function update() {
+    let newData = await fetch('http://localhost:8000/animals')
+      .then(response => response.json())
+      .then(json => {
+        if (json.error) {
+          console.log(json.message);
+          return;
+        }
+        return json;
+      });
+    setHeaders(newData.headers);
+    setDataRows(newData.data);
+  }
+
+  return <>
     <table className="stock-table" id="{ id }">
       <thead>
         <tr>
-          <th>Column header</th>
+          { headers.map((header, index) => (
+            <th key={index}>{header}</th>
+          ))}
         </tr>
       </thead>
       <tbody>
-        <tr>
-          <td>Column data</td>
-        </tr>
+        { dataRows.map((row, index) => (
+          <tr key={index}>
+            {row.map((dataPoint, dpIndex) => (
+              <td key={dpIndex}>{dataPoint}</td>
+            ))}
+          </tr>
+        ))
+        }
       </tbody>
     </table>
-    <FetchButton tableId="{ id }" />
-  </div>;
+    <button onClick={update}>Update</button>
+  </>;
 }
